afb4c0d97cfbd3f82b83193ffa7f882c4ce1624e
Arnoldj2 <74551278+Arnoldj2@users.noreply.github.com>
2021-03-22 10:50:44 -0600

    updates

--- Basic-JavaScript-Projects/JavaScript/JavaScript/JavaScript_3.js
+++
@@ -1,6 +0,0 @@
-function displayType(chest)  {
-
-  var chestType = chest.getAttribute("data-chest-type");    //assigned and called for data-attribute
-
-  alert(chestType + " are in the " + chest.innerHTML); 
-}
--- Basic-JavaScript-Projects/ToDo_App/CSS/todo.js
+++
@@ -1,7 +0,0 @@
-
-
-var mylist = { "name":"Jeff", "age":36, "city":"Salt Lake City" };
-
-var myJSON = JSON.stringify(mylist);
-
-document.getElementById('thislist').innerhtml =myJSON;
